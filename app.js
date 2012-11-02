f7a518b80e24417021ad43d7dba6f660b23eca9f
fabioberger <fabioberger1991@gmail.com>
2012-11-02 15:56:13 -0400

    remove dev mode

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ var app = module.exports = express();
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
-  app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.cookieParser('your secret here'));
