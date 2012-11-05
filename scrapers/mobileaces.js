9e47282c6158573a23255ad630c2350ded1c313e
Yang Su <suyang1+services@gmail.com>
2012-11-04 22:51:41 -0500

    Added documentation to mobileaces.js

--- scrapers/mobileaces.js
+++ scrapers/mobileaces.js
@@ -8,21 +8,25 @@ var _ = require('lodash');
 // Local Modules
 var parsers = require('./parsers');
 
-var data = {}
-  , queue = []
-  , index = 0;
+// Index of the current item being processed in the queue
+var index = 0;
 
+// initialized queue with the root page
+var queue = [{
+  type: 'base',
+  path: 'course_catalog.php?action=Catalog'
+}];
+
+// Cookie necessary to make https requests for aces
 var cookie = querystring.stringify({
   PHPSESSID: 'rej3a4u96kvl899r6mip8132o1'
 }, ';', '=');
 
-var base = {
-  type: 'base',
-  path: 'course_catalog.php?action=Catalog'
-};
-
-queue.push(base);
-
+/**
+ * Read the current item in the queue
+ * Makes https requests to fetch the next page if item.path exists
+ * @param  {object} item item containing parsed data and urls
+ */
 function fetch(item) {
   if (!item || !item.path) {
     process.nextTick(processNext);
@@ -32,12 +36,14 @@ function fetch(item) {
   var starttime = Date.now()
     , path;
 
+  // Process absolutel and relative links
   if (item.path[0] === '/') {
     path = item.path;
   } else {
     path = '/public_page/' + item.path
   }
 
+  // Https options
   var options = {
     host: 'm.siss.duke.edu',
     port: 443,
@@ -48,9 +54,8 @@ function fetch(item) {
     }
   };
 
+  // makes request and process the data once the download is complete
   var req = https.request(options, function(res) {
-    // console.log("statusCode: ", res.statusCode);
-    // console.log("headers: ", res.headers);
     var text = '';
 
     res.on('data', function(chunk) {
@@ -61,10 +66,12 @@ function fetch(item) {
       var result = parsers[item.type](text);
       queue = queue.concat(result);
       process.nextTick(processNext);
+
       console.log('fetched',item.path, 'in', (Date.now() - starttime)/1000, 's');
     });
 
   });
+
   req.end();
 
   req.on('error', function(e) {
@@ -72,13 +79,18 @@ function fetch(item) {
   });
 };
 
+/**
+ * Process the next step in the queue
+ * If there are no more items, write the data to a json file
+ */
 function processNext () {
   console.log('processNext', index, '/',queue.length);
+
   if (index < queue.length) {
     fetch(queue[index++]);
   } else {
     var fs = require('fs');
-    fs.writeFile('data.json', JSON.stringify(queue, null, 4), function(err) {
+    fs.writeFile('maces-'+Date.now()+'.json', JSON.stringify(queue, null, 4), function(err) {
         if (err) {
             console.log(err);
         } else {
@@ -89,4 +101,5 @@ function processNext () {
   }
 };
 
+// start processor
 processNext();
