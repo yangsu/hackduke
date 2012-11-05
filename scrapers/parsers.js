652377c977b07a3eb76d54baca491cec55b53816
Yang Su <suyang1+services@gmail.com>
2012-11-04 20:09:40 -0500

    Added parser for class pages

--- scrapers/parsers.js
+++ scrapers/parsers.js
@@ -6,19 +6,25 @@ function escapeRegex(regex) {
   return regex.replace(/([\/])/g, "\\$1");
 };
 
-function parseRegex(text, regex, labels) {
-  // match all, ignore case
-  var reg = new RegExp(escapeRegex(regex), 'gi');
-  var returnVal = [];
+function parseRegex(text, regex, labels, limit) {
+  // match all, ignore case, multiline
+  var reg = new RegExp(escapeRegex(regex), 'gim')
+    , returnVal = []
+    , matchCount = 0
+    , matchLimit = limit || 1;
   var matches, tempVal;
 
-  // while => if to limit to 1 link
-  if ((matches = reg.exec(text)) !== null) {
+  while ((matches = reg.exec(text)) !== null) {
+    if (matchCount >= matchLimit) {
+      break;
+    }
+
     tempVal = _.reduce(labels, function (memo, label, i) {
       memo[label] = matches[i + 1];
       return memo;
     }, {});
     returnVal.push(tempVal);
+    matchCount++;
   }
   return returnVal;
 };
@@ -53,6 +59,57 @@ parsers.classes = liParseGenerator(
 );
 
 parsers.class = function (text) {
+  var returnVal = {}
+    , tempLabel = 'content'
+    , uls = parseRegex(
+      text,
+      '<ul[^>]+>(.+?)(</ul>)',
+      [tempLabel],
+      3
+    );
+
+  returnVal.info = parseRegex(
+    text,
+    '<h3>([^<]+)</h3><p><strong>([^<]+)</strong></p><p>([^<]+)</p>',
+    ['number', 'title', 'description']
+  );
+
+  returnVal.details = parseRegex(
+    uls[0][tempLabel],
+    '<li[^>]*><h4>([^<]+)</h4><h4[^>]+>([^<]+)</h4',
+    ['label', 'value'],
+    10
+  );
+
+  returnVal.offering = parseRegex(
+    uls[1][tempLabel],
+    '<li[^>]*><h4>([^<]+)</h4><h4[^>]+>([^<]+)</h4',
+    ['label', 'value'],
+    10
+  );
+
+  if (uls[2]) {
+    returnVal.enrollmentReq = parseRegex(
+      uls[2][tempLabel],
+      '<li style[^>]*>([^<]+)</li>',
+      ['req'],
+      10
+    );
+  }
+
+  tempLabel = 'path';
+  returnVal.path = parseRegex(
+    text,
+    '<a data-role="button" href="([^"]+)"',
+    [tempLabel]
+  )[0][tempLabel];
+
+  returnVal.type = 'sections';
+
+  return returnVal;
+};
+
+parsers.sections = function (text) {
 
 };
 
