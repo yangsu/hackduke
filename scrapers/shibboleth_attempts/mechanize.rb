ed3fd07ceb52877a41b9cefd5421f4cf0048b914
fabioberger <fabioberger1991@gmail.com>
2012-11-05 01:17:43 -0500

    Working Mechanizer

--- scrapers/shibboleth_attempts/mechanize.rb
+++ scrapers/shibboleth_attempts/mechanize.rb
@@ -1,25 +1,38 @@
-# Using Mechanize in Ruby
-# ISSUE:  SCRIPT TIMES OUT WHEN TRYING TO GET LOGIN PAGE. ERROR UNKNOWN
-
 require 'rubygems'
 require 'mechanize'
+require 'logger'
 
-a = Mechanize.new
+a = Mechanize.new 
+a.user_agent_alias = 'Mac Safari'
+a.agent.redirect_ok = :all, true
+a.ssl_version = 'SSLv3'
+a.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+a.log = Logger.new "mech.log"
+
 a.get('https://www.siss.duke.edu/psp/CSPRD01/?cmd=start') do |page|
 
-  puts page.inspect
-
   # Submit the login form
-  my_page = login_page.form_with(:action => '/idp/authn/external') do |f|
+  my_page = page.form_with(:action => '/idp/authn/external') do |f|
     #f.j_username  = 'flb5' #ARGV[0]
-    f.field_with(:id => "j_username").value = 'NETID HERE'
+    f.field_with(:id => "j_username").value = 'flb5'
     #f.j_password  = 'handyArmsWursti7' #ARGV[1]
-    f.field_with(:id => "j_password").value = 'PASSWORD HERE'
+    f.field_with(:id => "j_password").value = 'handyArmsWursti7'
   end.submit
 
+  form = my_page.forms.first
+
+  final = form.submit
+
+  a.get('https://www.siss.duke.edu/psp/CSPRD01/EMPLOYEE/HRMS/h/?tab=DEFAULT') do |page|
+    puts page.body
+  end
+
+  '''
   my_page.links.each do |link|
     text = link.text.strip
     next unless text.length > 0
     puts text
   end
+  '''
 end
