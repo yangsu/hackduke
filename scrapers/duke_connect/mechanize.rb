9deff2e453309b994cd146af8d97909230166a14
fabioberger <fabioberger1991@gmail.com>
2012-11-05 01:49:14 -0500

    duke connect cleanup

---
+++ scrapers/duke_connect/mechanize.rb
@@ -0,0 +1,36 @@
+require 'rubygems'
+require 'mechanize'
+require 'logger'
+
+a = Mechanize.new 
+a.user_agent_alias = 'Mac Safari'
+a.agent.redirect_ok = :all, true
+a.ssl_version = 'SSLv3'
+a.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+a.log = Logger.new "mech.log"
+
+a.get('https://www.siss.duke.edu/psp/CSPRD01/?cmd=start') do |page|
+
+  # Submit the login form
+  my_page = page.form_with(:action => '/idp/authn/external') do |f|
+    f.field_with(:id => "j_username").value = 'USERID'
+    f.field_with(:id => "j_password").value = 'PASSWORD'
+  end.submit
+
+  form = my_page.forms.first
+
+  final = form.submit
+
+  a.get('https://www.siss.duke.edu/psp/CSPRD01/EMPLOYEE/HRMS/h/?cmd=getCachedPglt&pageletname=HC_STUDENT_CENTER_HOME&tab=DEFAULT&PORTALPARAM_COMPWIDTH=Narrow&ptlayout=N') do |page|
+    puts page.body
+  end
+
+  '''
+  my_page.links.each do |link|
+    text = link.text.strip
+    next unless text.length > 0
+    puts text
+  end
+  '''
+end
