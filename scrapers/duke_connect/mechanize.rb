a0613548a6f215331396e197ca18c0263965457f
fabioberger <fabioberger1991@gmail.com>
2012-11-05 02:30:59 -0500

    Duke connect returns courses in JSON

--- scrapers/duke_connect/mechanize.rb
+++ scrapers/duke_connect/mechanize.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'mechanize'
 require 'logger'
+require 'json'
 
 a = Mechanize.new 
 a.user_agent_alias = 'Mac Safari'
@@ -14,17 +15,27 @@ a.get('https://www.siss.duke.edu/psp/CSPRD01/?cmd=start') do |page|
 
   # Submit the login form
   my_page = page.form_with(:action => '/idp/authn/external') do |f|
-    f.field_with(:id => "j_username").value = 'USERID'
-    f.field_with(:id => "j_password").value = 'PASSWORD'
+    f.field_with(:id => "j_username").value = ARGV[0]
+    f.field_with(:id => "j_password").value = ARGV[1]
   end.submit
 
   form = my_page.forms.first
 
   final = form.submit
 
+
+  courses = Array.new()
   a.get('https://www.siss.duke.edu/psp/CSPRD01/EMPLOYEE/HRMS/h/?cmd=getCachedPglt&pageletname=HC_STUDENT_CENTER_HOME&tab=DEFAULT&PORTALPARAM_COMPWIDTH=Narrow&ptlayout=N') do |page|
-    puts page.body
+    page.search('span[@class="PSHYPERLINKDISABLED"][@title="View Details"]').each do |link|
+      course_info = link.content
+      course_info["\n"]= " "
+      courses << course_info
+      #puts link.content
+    end
   end
+
+  puts courses.to_json
+
 
   '''
   my_page.links.each do |link|
