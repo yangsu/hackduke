103260b59ada6495e02ac2d34bc3175c293d9b14
Yang Su <suyang1+services@gmail.com>
2013-04-01 01:32:09 -0400

    Updated evaluations to scrape from where it left off

--- scrapers/evaluations.js
+++ scrapers/evaluations.js
@@ -5,12 +5,17 @@ var config = require('./config');
 
 db.Term.find({}, { course_id: 1 }, { lean: true }, function(err, terms) {
   var uniqIds = _.unique(_.pluck(terms, 'course_id'));
-  var ts = _.map(uniqIds, function(t) {
-    return {
-      type: 'evaluation',
-      path: config.EVALURL + 'list.php?crse_id=' + t
-    };
-  });
 
-  db.parallel(ts, 'Evaluation');
+  db.Evaluation.find({}, { course_id: 1 }, { lean: true }, function(err, evals) {
+    uniqIds = _.difference(uniqIds, _.pluck(evals, 'course_id'));
+
+    var ts = _.map(uniqIds, function(t) {
+      return {
+        type: 'evaluation',
+        path: config.EVALURL + 'list.php?crse_id=' + t
+      };
+    });
+
+    db.parallel(ts, 'Evaluation');
+  });
 });
