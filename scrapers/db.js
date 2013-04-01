aa6d8bbaa578206493cf122bf8aa5baba3de64a0
Yang Su <suyang1+services@gmail.com>
2013-03-31 22:30:24 -0400

    Refactoring and styling changes for db.js

--- scrapers/db.js
+++ scrapers/db.js
@@ -11,34 +11,35 @@ var queryMap = {};
 mongoose.connect('localhost', 'aces');
 
 var CourseNumberMappingSchema = mongoose.Schema({
-  department:       String,
+  department: String,
   department_title: String,
-  course_title:     String,
-  old_number:       String,
-  new_number:       String
+  course_title: String,
+  old_number: String,
+  new_number: String
 });
 
 CourseNumberMappingSchema.index({
-  department:       1,
+  department: 1,
   department_title: 1,
-  course_title:     1,
-  old_number:       1,
-  new_number:       1
+  course_title: 1,
+  old_number: 1,
+  new_number: 1
 });
 
 db.CourseNumberMapping = mongoose.model(
-  'CourseNumberMapping',
-  CourseNumberMappingSchema
-);
+    'CourseNumberMapping',
+    CourseNumberMappingSchema
+    );
 
 queryMap.CourseNumberMapping = function(q) {
-  return {
-    department: q.department,
-    department_title: q.department_title,
-    course_title: q.course_title,
-    old_number: q.old_number,
-    new_number: q.new_number
-  };
+  return _.pick(
+      q,
+      'department',
+      'department_title',
+      'course_title',
+      'old_number',
+      'new_number'
+  );
 };
 
 var DepartmentSchema = mongoose.Schema({
@@ -53,17 +54,14 @@ DepartmentSchema.index({
 
 db.Department = mongoose.model('department', DepartmentSchema);
 queryMap.Department = function(q) {
-  return {
-    code: q.code,
-    title: q.title
-  };
+  return _.pick(q, 'code', 'title');
 };
 
 var ClassSchema = mongoose.Schema({
   department: String,
-  number:     String,
-  title:      String,
-  path:       String
+  number: String,
+  title: String,
+  path: String
 }, {
   strict: false
 });
@@ -75,18 +73,15 @@ ClassSchema.index({
 
 db.Class = mongoose.model('class', ClassSchema);
 queryMap.Class = function(q) {
-  return {
-    department: q.department,
-    number: q.number
-  };
+  return _.pick(q, 'departments', 'number');
 };
 
 var TermSchema = mongoose.Schema({
   department: String,
-  number:     String,
-  course_id:  String,
-  title:      String,
-  path:       String
+  number: String,
+  course_id: String,
+  title: String,
+  path: String
 }, {
   strict: false
 });
@@ -99,22 +94,18 @@ TermSchema.index({
 
 db.Term = mongoose.model('Term', TermSchema);
 queryMap.Term = function(q) {
-  return {
-    department: q.department,
-    number: q.number,
-    course_id: q.course_id
-  };
+  return _.pick(q, 'department', 'number', 'course_id');
 };
 
 var SectionSchema = mongoose.Schema({
   department: String,
-  number:     String,
-  course_id:  String,
-  term_id:    String,
-  class_id:   String,
+  number: String,
+  course_id: String,
+  term_id: String,
+  class_id: String,
   section_id: String,
-  title:      String,
-  path:       String
+  title: String,
+  path: String
 }, {
   strict: false
 });
@@ -130,14 +121,7 @@ SectionSchema.index({
 
 db.Section = mongoose.model('Section', SectionSchema);
 queryMap.Section = function(q) {
-  return {
-    department: q.department,
-    number: q.number,
-    course_id: q.course_id,
-    term_id: q.term_id
-    // class_id: q.class_id,
-    // section_id: q.section_id
-  };
+  return _.pick(q, 'department', 'number', 'course_id', 'term_id');
 };
 
 
@@ -166,13 +150,8 @@ LocationSchema.index({
 
 db.Location = mongoose.model('Location', LocationSchema);
 queryMap.Location = function(q) {
-  return {
-    id: q.id,
-    school_id: q.school_id,
-    school_building_id:q.school_building_id
-  };
+  return _.pick(q, 'id', 'school_id', 'school_building_id');
 };
-
 
 var parsers = require('./cheerioparser');
 var utils = require('./utils');
@@ -186,32 +165,31 @@ function parallel(collection, model, finalCallback) {
         if (error) {
           callback(error);
         } else {
-          // try {
-            var parsed = parsers[item.type](text);
+          var parsed = parsers[item.type](text);
 
-            var qsData = qs.parse(item.path.split('?')[1]);
-            if (qsData.subject) qsData.department = qsData.subject;
-            if (qsData.class) qsData.number = qsData.class;
-            if (qsData.openTerms) qsData.course_id = qsData.openTerms;
-            if (qsData.crse_id) qsData.course_id = qsData.crse_id;
-            if (qsData.openSections) qsData.term_id = qsData.openSections;
-            if (qsData.strm) qsData.term_id = qsData.strm;
-            if (qsData.class_nbr) qsData.class_id = qsData.class_nbr;
-            if (qsData.section) qsData.section_id = qsData.section;
+          var qsData = qs.parse(item.path.split('?')[1]);
+          if (qsData.subject) qsData.department = qsData.subject;
+          if (qsData.class) qsData.number = qsData.class;
+          if (qsData.openTerms) qsData.course_id = qsData.openTerms;
+          if (qsData.crse_id) qsData.course_id = qsData.crse_id;
+          if (qsData.openSections) qsData.term_id = qsData.openSections;
+          if (qsData.strm) qsData.term_id = qsData.strm;
+          if (qsData.class_nbr) qsData.class_id = qsData.class_nbr;
+          if (qsData.section) qsData.section_id = qsData.section;
 
-            qsData = _.pick(qsData, 'department', 'number', 'course_id', 'term_id', 'class_id', 'section_id');
+          qsData = _.pick(qsData, 'department', 'number', 'course_id', 'term_id', 'class_id', 'section_id');
 
-            if (_.isArray(parsed)) {
-              parsed = _.map(parsed, function(p) {
-                return _.extend(p, qsData);
-              });
-            } else if (_.isObject(parsed)) {
-              _.extend(parsed, qsData);
-            }
+          if (_.isArray(parsed)) {
+            parsed = _.map(parsed, function(p) {
+              return _.extend(p, qsData);
+            });
+          } else if (_.isObject(parsed)) {
+            _.extend(parsed, qsData);
+          }
 
-            var genDbRequest = function (d) {
-              return function(cb) {
-                db[model].update(
+          var genDbRequest = function(d) {
+            return function(cb) {
+              db[model].update(
                   queryMap[model](d),
                   { $set: d },
                   { upsert: true },
@@ -219,50 +197,46 @@ function parallel(collection, model, finalCallback) {
                     if (err) err.item = d;
                     cb(err, data);
                   }
-                );
-                return d;
-              };
+              );
+              return d;
             };
-            var logProgress = function(data) {
-              console.log(
-                '(', count++, '/', collection.length, '-',
-                (count/collection.length * 100 + '').slice(0, 4) + '%',')',
+          };
+          var logProgress = function(data) {
+            var pct = (count / collection.length * 100 + '').slice(0, 4) + '%';
+            console.log(
+                '(', count++, '/', collection.length, '-', pct, ')',
                 'Fetched and Saved ', _.isArray(data) ? data.length : 1,
                 ' items from ', item.path,
-                'in', timing.totaltime, 's'
-              );
-            };
+                'in', timing && timing.totaltime, 's'
+            );
+          };
 
-            if (_.isArray(parsed)) {
-              var dbRequests = _.map(parsed, function(d) {
-                return genDbRequest(d);
-              });
+          if (_.isArray(parsed)) {
+            var dbRequests = _.map(parsed, function(d) {
+              return genDbRequest(d);
+            });
 
-              async.parallel(dbRequests, function(err, data) {
-                logProgress(data);
-                callback(err, data);
-              });
-            } else if (_.isObject(parsed)) {
-              genDbRequest(parsed)(function(err, data) {
-                logProgress(data);
-                callback(err, data);
-              })
-            } else {
-              callback(parsed);
-            }
-
-          // } catch (e) {
-          //   callback(e);
-          // }
+            async.parallel(dbRequests, function(err, data) {
+              logProgress(data);
+              callback(err, data);
+            });
+          } else if (_.isObject(parsed)) {
+            genDbRequest(parsed)(function(err, data) {
+              logProgress(data);
+              callback(err, data);
+            });
+          } else {
+            callback(parsed);
+          }
         }
       });
     };
   });
 
   async.parallel(requests, finalCallback);
-};
+}
 
-db.parallel = function (collection, model, finalCallback) {
+db.parallel = function(collection, model, finalCallback) {
   var chunks = utils.toChunks(collection, config.CHUNKSIZE);
   var chunkIndex = 0;
 
