e04aea505c9a85cb1aa772e41e860fe995237c65
Yang Su <suyang1+services@gmail.com>
2013-04-01 01:46:34 -0400

    Filter out evaluations that already have details

--- scrapers/evaluationdetail.js
+++ scrapers/evaluationdetail.js
@@ -3,7 +3,9 @@ var _ = require('lodash');
 var db = require('./db');
 var config = require('./config');
 
-db.Evaluation.find({}, { detailPath: 1 }, { lean: true }, function(err, terms) {
+db.Evaluation.find({
+  details: { $exists: false }
+}, { detailPath: 1 }, { lean: true }, function(err, terms) {
   var paths = _.pluck(terms, 'detailPath');
   var ts = _.map(paths, function(p) {
     return {
