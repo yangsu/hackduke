3f7103ea800baa52fee6a47b64e5df0c7c93c173
Yang Su <suyang1+services@gmail.com>
2013-04-02 13:54:58 -0400

    Removed json from api urls

--- api/api.js
+++ api/api.js
@@ -31,23 +31,23 @@ server.use(restify.throttle({
 
 var routes = require('./routes');
 
-server.get('/list.json/department', routes.listdepartment);
-server.get('/list.json/department/:department', routes.listclass);
-server.get('/list.json/department/:department/class/:number', routes.listterm);
-server.get('/list.json/department/:department/class/:number/term/:title', routes.listsection);
+server.get('/list/department', routes.listdepartment);
+server.get('/list/department/:department', routes.listclass);
+server.get('/list/department/:department/class/:number', routes.listterm);
+server.get('/list/department/:department/class/:number/term/:title', routes.listsection);
 
-server.get('/department.json', routes.departments);
-server.get('/department.json/:id', routes.departmentById);
+server.get('/departments', routes.departments);
+server.get('/department/:id', routes.departmentById);
 
-server.get('/class.json/department/:department/class/:number', routes.class);
-server.get('/class.json/:id', routes.classById);
+server.get('/class/department/:department/class/:number', routes.class);
+server.get('/class/:id', routes.classById);
 
-server.get('/class.json', routes.classes);
+server.get('/classes', routes.classes);
 
-server.get('/classes.json/department/:department', routes.classes);
+server.get('/classes/department/:department', routes.classes);
 
-server.get('/evaluation.json/department/:department/class/:number', routes.evaluation);
-// server.get('/evaluation.json/course_id/:cid/class_id/:section', routes.evaluation);
+server.get('/evaluation/department/:department/class/:number', routes.evaluation);
+// server.get('/evaluation/course_id/:cid/class_id/:section', routes.evaluation);
 
 
 server.listen(8080, function() {
