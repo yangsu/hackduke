afd8528666da00436e19794d93939d4fc1a668e3
Yang Su <suyang1+services@gmail.com>
2013-04-06 02:00:38 -0400

    Added file to fix ids

---
+++ scrapers/fixes/parsePaths.js
@@ -0,0 +1,42 @@
+var _ = require('lodash');
+var async = require('async');
+
+var db = require('../db');
+
+var wrapError = function(cb) {
+  return function(err, data) {
+    if (err) {
+      console.log('ERROR', err);
+    } else {
+      cb(data);
+    }
+  }
+};
+
+var dbParsePath = function(collection, path) {
+  return function(callback) {
+    var query = {};
+    query[path] = { $exists: true, $ne: null };
+    db[collection].find(query, {}, { limit: 0 }, wrapError(function(d) {
+      var reqs = _.map(d, function(doc) {
+        return function(cb) {
+          doc.set(db.parseQSData(doc.get(path)));
+          doc.save(cb);
+        };
+      });
+      async.parallel(reqs, callback);
+    }));
+  };
+};
+
+async.series({
+  // class: dbParsePath('Class', 'sectionsPath'),
+  // term: dbParsePath('Term', 'path'),
+  section: dbParsePath('Section', 'path'),
+  evaluation: dbParsePath('Evaluation', 'detailPath')
+}, function(err, data) {
+  _.each(data, function(v, k) {
+    console.log(k, v.length);
+  });
+  process.exit(0);
+});
