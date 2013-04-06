f6f673e1510cfd495b4f3fb7d6ddbe2e123c1f04
Yang Su <suyang1+services@gmail.com>
2013-04-06 14:43:27 -0400

    Added scraped dir script

---
+++ scrapers/directory/scrapedir.sh
@@ -0,0 +1,9 @@
+
+YEAR=${1:-2013}
+FILE=$YEAR.txt
+
+for i in {a..z}{a..z}; do
+  echo $i;
+  ldapsearch -H ldap://ldap.duke.edu -LLL -b 'ou=People,dc=duke,dc=edu' -z 0 -l 0 -x "(&(duPSExpGradTermC1=$YEAR*)(uid=$i*)(eduPersonAffiliation=student))" > $FILE;
+  node dir.js $FILE
+done
