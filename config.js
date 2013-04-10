8e178364ae6c063dbdaff8494989bb53e4c0bf95
Yang Su <suyang1+services@gmail.com>
2013-04-10 16:19:36 -0400

    Added other uris for mongo

--- config.js
+++ config.js
@@ -3,5 +3,7 @@ module.exports = {
   version: '0.1.0',
   swaggerVersion: '1.1',
   basePath: 'http://colab-sbx-03.oit.duke.edu:8080/',
-  mongouri: 'mongodb://localhost/aces'
+  // mongouri: 'mongodb://localhost/aces'
+  mongouri: 'mongodb://hackduke:T6ct5YhUvsaK@ds029117.mongolab.com:29117/heroku_app14811010'
+  // mongouri: 'mongodb://colab-sbx-03.oit.duke.edu/hackduke'
 };
