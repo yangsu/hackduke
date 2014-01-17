0c9c00b56db20d97f6efe7c738359001eac4d49f
Yang Su <suyang1+services@gmail.com>
2014-01-17 13:46:29 -0800

    Changed base url

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'Duke API',
   version: '0.1.0',
   swaggerVersion: '1.1',
-  basePath: 'http://www.hackduke.com/',
+  basePath: 'http://hackduke.herokuapp.com/',
   // mongouri: 'mongodb://localhost/aces'
   mongouri: 'mongodb://hackduke:T6ct5YhUvsaK@ds029117.mongolab.com:29117/heroku_app14811010'
   // mongouri: 'mongodb://colab-sbx-03.oit.duke.edu/hackduke'
